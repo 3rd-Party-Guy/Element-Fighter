d05c215cb7368bc37993a3708d86f1285ed760b8
Leon Enders <m.enders.leon@gmail.com>
2024-02-14 15:49:07 +0100

    removed InputMappingContext, implemented addInputActionLookUp

--- Assets/Scripts/Game.js
+++ Assets/Scripts/Game.js
@@ -1,9 +1,9 @@
 // Author:          Nikolay Hadzhiev
 // Description:     This is the game entry-point
 
+import MoveCommand from "./Command.js";
 import Entity from "./Entity.js";
 import InputManager from "./InputManager.js";
-import { InputMappingContext } from "./InputManager.js";
 
 const canvas = document.getElementById('game-window');
 const ctx = canvas.getContext('2d');
@@ -11,11 +11,9 @@ const ctx = canvas.getContext('2d');
 let player = new Entity(75, 75, "Assets/Sprites/Knight - Debug/Knight.png", ctx);
 
 
-// Create InputMappingContext, mapping InputKeys to InputActions
-let iMC = new InputMappingContext(65,68,32,17,81,87, 69, 82);
-
-let inputManager = new InputManager(player, iMC);
-
+let inputManager = new InputManager(player);
+inputManager.addInputActionLookUp(65, new MoveCommand(-1,0));
+inputManager.addInputActionLookUp(68, new MoveCommand(1,0));
 window.addEventListener('keydown', (event)=>{inputManager.handleInput(event.keyCode)} ) ;
 
 
--- Assets/Scripts/InputManager.js
+++ Assets/Scripts/InputManager.js
@@ -2,29 +2,6 @@
 /// Description:    This is the InputManager Singleton
 import MoveCommand from "./Command.js"
 
-export  class InputMappingContext
-{
-    walkLeft = 0;
-    walkRight = 0;
-    jump = 0;
-    duck = 0;
-    attack1 = 0;
-    attack2 = 0;
-    ability1 = 0;
-    ability2 = 0;
-
-    constructor(leftKey,rightKey,jumpKey, duckKey, attack1Key, attack2Key, ability1Key,ability2Key)
-    {
-        this.walkLeft = leftKey;
-        this.walkRight = rightKey;
-        this.jump = jumpKey;
-        this.duck = duckKey;
-        this.attack1 = attack1Key;
-        this.attack2 = attack2Key;
-        this.ability1 = ability1Key;
-        this.ability2 = ability2Key;
-    }
-}
 
 
 
@@ -32,35 +9,31 @@ export default class InputManager{
     // Instance Properties: ------------------------------------------------
     // Player variable
     player = undefined;
-    
-    //Input Mapping Context , mapping InputAction to InputKey
-    inputMappingContext = undefined;
 
     // Mapping InputKeyCode to Command
     inputActionLookUp = new Map();
 
-   
-    
-    
-
     
     // Constructor: ---------------------------------------------------------
-    constructor(in_player, in_inputMappingContext )
+    constructor(in_player )
     {
         this.player = in_player;
-        this.inputMappingContext = in_inputMappingContext;
-        this.inputActionLookUp.set(this.inputMappingContext.walkLeft, new MoveCommand(-1,0));
-        this.inputActionLookUp.set(this.inputMappingContext.walkRight, new MoveCommand(1,0));
     }
 
     handleInput(keyCode)
     {
         console.log("event keycode: " + keyCode);
-        if(this.inputActionLookUp.get(keyCode) != null)
+        let inputKeyLookUp = this.inputActionLookUp.get(keyCode);
+        if(inputKeyLookUp != null)
         {
-            this.inputActionLookUp.get(keyCode).execute(this.player);
+            inputKeyLookUp.execute(this.player);
         }
         
+    }
+
+    addInputActionLookUp(keyCode, Command)
+    {
+        this.inputActionLookUp.set(keyCode, Command);
     }
 
 }
