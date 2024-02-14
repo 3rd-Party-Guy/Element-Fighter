a8b620b527332a6a937706ed02aacc050082994b
Leon Enders <m.enders.leon@gmail.com>
2024-02-14 14:47:13 +0100

    create InputManager, InputMappingContext, added InputHandling for KeyPress

--- Assets/Scripts/Game.js
+++ Assets/Scripts/Game.js
@@ -2,11 +2,22 @@
 // Description:     This is the game entry-point
 
 import Entity from "./Entity.js";
+import InputManager from "./InputManager.js";
+import { InputMappingContext } from "./InputManager.js";
 
 const canvas = document.getElementById('game-window');
 const ctx = canvas.getContext('2d');
 
 let player = new Entity(75, 75, "Assets/Sprites/Knight - Debug/Knight.png", ctx);
+
+
+// Create InputMappingContext, mapping InputKeys to InputActions
+let iMC = new InputMappingContext(65,68,32,17,81,87, 69, 82);
+
+let inputManager = new InputManager(player, iMC);
+
+window.addEventListener('keydown', (event)=>{inputManager.handleInput(event.keyCode)} ) ;
+
 
 // This is the main game loop. It is called every frame!
 function GameLoop() {
