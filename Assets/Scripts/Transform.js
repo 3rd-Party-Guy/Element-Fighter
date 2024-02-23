d9f86b6f06cce3fedfd95546819ca382e7acd669
Leon Enders <m.enders.leon@gmail.com>
2024-02-23 13:41:01 +0100

    Created Transform Class

---
+++ Assets/Scripts/Transform.js
@@ -0,0 +1,48 @@
+export default class Transform{
+
+    position = new Vector2(0,0);
+    rotation = new Vector2(0,0);
+    scale = new Vector2(0,0);
+
+    constructor(position, rotation, scale)
+    {
+        
+        this.position = position;
+        this.rotation = rotation;
+        this.scale = scale;
+    }
+
+
+    get position() { return this.position; }
+    get rotation() { return this.rotation; }
+    get scale() { return this.scale; }
+
+    set position(position) { this.position = position; }
+    set rotation(rotation) { this.rotation = rotation; }
+    set scale(scale) { this.scale = scale; }
+
+    set (transform) {
+        this.position = transform.position;
+        this.rotation = transform.rotation;
+        this.scale = transform.scale;
+    }
+
+    add (transform){
+        this.position += transform.position;
+        this.rotation += transform.rotation;
+        this.scale += transform.scale;
+    }
+
+    subtract(transform){
+        this.position -= transform.position;
+        this.rotation -= transform.rotation;
+        this.scale -= transform.scale;
+    }
+
+    multiply(scalar){
+        this.position *= scalar;
+        this.rotation *= scalar;
+        this.scale *= scalar;
+    }
+
+}
