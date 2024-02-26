e91b52571d7e8fafd5b6e19da31538a27421cf23
3rd-Party-Guy <grochobg@gmail.com>
2024-02-26 11:23:51 +0100

    Remove Redundancy

--- Assets/Scripts/Components/Component.js
+++ Assets/Scripts/Components/Component.js
@@ -1,8 +1,6 @@
 /// Author:         Leon Enders, Nikolay Hadzhiev
 /// Description:    An abstract class for the Components in the Entity Component System followed by concrete classes of inerhiting from component afterwards. These are used to manage behaviour of entities, for different game domains.
 
-
-
 // Base Class for Components, shared behaviour for all components will be implemented here
 // implements behaviour for a specific domain to decouple from entity
 // Create Child classes for each domains behaviour, to decouple most of the behaviour from entity (Currently: Transform Component, Physics Component, Rendering Component)
