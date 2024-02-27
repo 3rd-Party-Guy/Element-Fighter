6e10e0b34aedc3ed1e9612f7a83453a95a5e76ed
3rd-Party-Guy <grochobg@gmail.com>
2024-02-27 11:33:39 +0100

    Gamepad Support

--- Assets/Scripts/Singletons/InputManager.js
+++ Assets/Scripts/Singletons/InputManager.js
@@ -2,51 +2,86 @@
 /// Description:    Keeps track of button presses, routes Commands every frame
 
 import Singleton from "./Singleton.js";
-import Command from "../Command.js"
 import EntityManager from "./EntityManager.js";
-import Entity from "../Entity.js";
+import { GamepadMoveHorizontalCommand, GamepadJumpCommand, GamepadDuckCommand } from "../CommandGamepad.js";
 
 export default class InputManager extends Singleton {
     // Lookup Table for input codes and Commands
-    inputActionLookUp = undefined;
-    
-    activeInputLookup = undefined;
+    inputKeyboardActionLookup = undefined;
+    activeKeyboardInputLookup = undefined;
+
+    gamepad_horizontal_command = new GamepadMoveHorizontalCommand();
+    gamepad_jump_command = new GamepadJumpCommand();
+    gamepad_duck_command = new GamepadDuckCommand();
+    gamepad_holding_jump = false;
+    gamepad_holding_duck = false;
 
     constructor() {
         super();
 
-        this.inputActionLookup = new Map();
-        this.activeInputLookup = new Map();
+        this.inputKeyboardActionLookup = new Map();
+        this.activeKeyboardInputLookup = new Map();
     }
 
+    /// ----    KEYBOARD INPUT BEGIN    ----
     // Set Input is called every time there is an input event.
     // This is used to keep track of all keys that are currently down.
-    setInput(event) {
+    setKeyboardInput(event) {
         const is_active = event.type == 'keydown';
-        const command = this.inputActionLookup.get(event.code);
+        const command = this.inputKeyboardActionLookup.get(event.code);
 
-        this.activeInputLookup.set(event.code, is_active);
-
-        if (is_active)
-            command?.onSet();
-        else
-            command?.onUnset();
+        this.activeKeyboardInputLookup.set(event.code, is_active);
+        
+        (is_active) ? command?.onSet() : command?.onUnset();
     }
 
     // Handle Input is called every frame. It iterates through all commands map to
     // a key code that is held down
-    handleInput()
+    handleKeyboardInput()
     {
-        for (const [key, value] of this.activeInputLookup.entries())
+        for (const [key, value] of this.activeKeyboardInputLookup.entries())
             if (value === true)
-                this.inputActionLookup.get(key)?.execute(EntityManager.getInstance(EntityManager).playerOne);
+                this.inputKeyboardActionLookup.get(key)?.execute(EntityManager.getInstance(EntityManager).playerOne);
     }
 
     // Adds or changes an existing keyCode's Command
-    addInputActionLookup = (keyCode, Command) =>
-        this.inputActionLookup.set(keyCode, Command);
+    addKeyboardInputActionLookup = (keyCode, command) =>
+        this.inputKeyboardActionLookup.set(keyCode, command);
     
     isKeyActive = (keyCode) => {
-        return this.activeInputLookup.get(keyCode) ? true : false;
+        return this.activeKeyboardInputLookup.get(keyCode) ? true : false;
+    }
+    /// ----    KEYBOARD INPUT END  ----
+
+    handleGamepadInput() {
+        const gp1 = navigator.getGamepads()[0];
+        if (!gp1) return;
+        const playerOne = EntityManager.getInstance(EntityManager).playerOne;
+
+        this.gamepad_horizontal_command.execute(playerOne, gp1["axes"][0])
+
+        this.handleGamepadJump(gp1, playerOne);
+        this.handleGamepadDuck(gp1, playerOne);
+    }
+
+    handleGamepadJump(gamepad, player) {
+        if (gamepad["buttons"][3].pressed) {
+            if (!this.gamepad_holding_jump) {
+                this.gamepad_jump_command.execute(player);
+                this.gamepad_holding_jump = true;
+            }
+        } else
+            this.gamepad_holding_jump = false;
+    }
+
+    handleGamepadDuck(gamepad, player) {
+        if (gamepad["buttons"][2].pressed) {
+            if (!this.gamepad_holding_duck) {
+                this.gamepad_duck_command.execute(player);
+                this.gamepad_holding_duck = true;
+            }
+        } else 
+            this.gamepad_holding_duck = false;
+
     }
 }
