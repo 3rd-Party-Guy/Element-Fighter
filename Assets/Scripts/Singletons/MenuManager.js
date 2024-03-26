a956b282317f60e84a237b184297f4b03d113a0c
Leon Enders <m.enders.leon@gmail.com>
2024-03-26 14:59:52 +0100

    fixed buttons

--- Assets/Scripts/Singletons/MenuManager.js
+++ Assets/Scripts/Singletons/MenuManager.js
@@ -36,83 +36,117 @@ export default class MenuManager extends Singleton {
 
   selectUp(){
     if(!this.current_button) return;
-    let lastdistancesqrd = 0;
-    for(button of this.buttons)
+    let lastdistancesqrd = 999;
+    let next_button = undefined;
+    let validButtons = [];
+
+
+    for(const button of this.buttons)
     {
+      if(button === this.current_button)continue;
       if(button.y < this.current_button.y){
-        this.current_button = button;
-        continue;
+       validButtons.push(button);
       }
-      if(button.y === this.current_button.y){
+    }
+
+    
+    for( const button of validButtons)
+    {
         let distancesqrd = Math.sqrt(this.current_button.x - button.x) + Math.sqrt(this.current_button.y - button.y);
         if(distancesqrd < lastdistancesqrd){
           lastdistancesqrd = distancesqrd;
-          this.current_button = button;
+          next_button = button;
         }
-      }
+      
     }
+    this.current_button = next_button;
+    this.current_button.onSelect();
   }
 
   selectDown(){
     if(!this.current_button) return;
-    let lastdistancesqrd = 0;
-    for(button of this.buttons)
+    let lastdistancesqrd = 999;
+    let next_button = undefined;
+    let validButtons = [];
+
+
+    for(const button of this.buttons)
     {
+      if(button === this.current_button)continue;
       if(button.y > this.current_button.y){
-        this.current_button = button;
-        this.current_button.onSelect();
-        continue;
+       validButtons.push(button);
       }
-      if(button.y === this.current_button.y){
+    }
+
+    
+    for( const button of validButtons)
+    {
         let distancesqrd = Math.sqrt(this.current_button.x - button.x) + Math.sqrt(this.current_button.y - button.y);
         if(distancesqrd < lastdistancesqrd){
           lastdistancesqrd = distancesqrd;
-          this.current_button = button;
-          this.current_button.onSelect();
+          next_button = button;
         }
-      }
+      
     }
+    this.current_button = next_button;
+    this.current_button.onSelect();
   }
 
   selectLeft(){
     if(!this.current_button) return;
-    let lastdistancesqrd = 0;
-    for(button of this.buttons)
+    let lastdistancesqrd = 999;
+    let next_button = undefined;
+    let validButtons = [];
+
+
+    for(const button of this.buttons)
     {
+      if(button === this.current_button)continue;
       if(button.x < this.current_button.x){
-        this.current_button = button;
-        this.current_button.onSelect();
-        continue;
+       validButtons.push(button);
       }
-      if(button.x === this.current_button.x){
+    }
+
+    
+    for( const button of validButtons)
+    {
         let distancesqrd = Math.sqrt(this.current_button.x - button.x) + Math.sqrt(this.current_button.y - button.y);
         if(distancesqrd < lastdistancesqrd){
           lastdistancesqrd = distancesqrd;
-          this.current_button = button;
-          this.current_button.onSelect();
+          next_button = button;
         }
-      }
+      
     }
+    this.current_button = next_button;
+    this.current_button.onSelect();
   }
 
   selectRight(){
     if(!this.current_button) return;
-    let lastdistancesqrd = 0;
-    for(button of this.buttons)
+    let lastdistancesqrd = 999;
+    let next_button = undefined;
+    let validButtons = [];
+
+
+    for(const button of this.buttons)
     {
+      if(button === this.current_button)continue;
       if(button.x > this.current_button.x){
-        this.current_button = button;
-        this.current_button.onSelect();
-        continue;
+       validButtons.push(button);
       }
-      if(button.x === this.current_button.x){
+    }
+
+    
+    for( const button of validButtons)
+    {
         let distancesqrd = Math.sqrt(this.current_button.x - button.x) + Math.sqrt(this.current_button.y - button.y);
         if(distancesqrd < lastdistancesqrd){
           lastdistancesqrd = distancesqrd;
-          this.current_button = button;
-          this.current_button.onSelect();
+          next_button = button;
         }
-      }
+      
     }
+    this.current_button = next_button;
+    this.current_button.onSelect();
   }
 }
