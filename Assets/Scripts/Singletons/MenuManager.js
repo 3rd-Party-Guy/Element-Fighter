662ca973682600b3a190bf26df20c9929d3a4958
Leon Enders <m.enders.leon@gmail.com>
2024-03-26 13:23:48 +0100

    added directional select functions

--- Assets/Scripts/Singletons/MenuManager.js
+++ Assets/Scripts/Singletons/MenuManager.js
@@ -33,4 +33,86 @@ export default class MenuManager extends Singleton {
     this.buttons = {};
     this.current_button = {};
   }
+
+  selectUp(){
+    if(!this.current_button) return;
+    let lastdistancesqrd = 0;
+    for(button of this.buttons)
+    {
+      if(button.y < this.current_button.y){
+        this.current_button = button;
+        continue;
+      }
+      if(button.y === this.current_button.y){
+        let distancesqrd = Math.sqrt(this.current_button.x - button.x) + Math.sqrt(this.current_button.y - button.y);
+        if(distancesqrd < lastdistancesqrd){
+          lastdistancesqrd = distancesqrd;
+          this.current_button = button;
+        }
+      }
+    }
+  }
+
+  selectDown(){
+    if(!this.current_button) return;
+    let lastdistancesqrd = 0;
+    for(button of this.buttons)
+    {
+      if(button.y > this.current_button.y){
+        this.current_button = button;
+        this.current_button.onSelect();
+        continue;
+      }
+      if(button.y === this.current_button.y){
+        let distancesqrd = Math.sqrt(this.current_button.x - button.x) + Math.sqrt(this.current_button.y - button.y);
+        if(distancesqrd < lastdistancesqrd){
+          lastdistancesqrd = distancesqrd;
+          this.current_button = button;
+          this.current_button.onSelect();
+        }
+      }
+    }
+  }
+
+  selectLeft(){
+    if(!this.current_button) return;
+    let lastdistancesqrd = 0;
+    for(button of this.buttons)
+    {
+      if(button.x < this.current_button.x){
+        this.current_button = button;
+        this.current_button.onSelect();
+        continue;
+      }
+      if(button.x === this.current_button.x){
+        let distancesqrd = Math.sqrt(this.current_button.x - button.x) + Math.sqrt(this.current_button.y - button.y);
+        if(distancesqrd < lastdistancesqrd){
+          lastdistancesqrd = distancesqrd;
+          this.current_button = button;
+          this.current_button.onSelect();
+        }
+      }
+    }
+  }
+
+  selectRight(){
+    if(!this.current_button) return;
+    let lastdistancesqrd = 0;
+    for(button of this.buttons)
+    {
+      if(button.x > this.current_button.x){
+        this.current_button = button;
+        this.current_button.onSelect();
+        continue;
+      }
+      if(button.x === this.current_button.x){
+        let distancesqrd = Math.sqrt(this.current_button.x - button.x) + Math.sqrt(this.current_button.y - button.y);
+        if(distancesqrd < lastdistancesqrd){
+          lastdistancesqrd = distancesqrd;
+          this.current_button = button;
+          this.current_button.onSelect();
+        }
+      }
+    }
+  }
 }
